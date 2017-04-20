ea3e51b06cc0951f1fd0d203e273ba8e90517483
Igor Konovalov <konovalov.avp@gmail.com>
2017-04-20 20:53:14 +0300

    login functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import webdriver, {By, until} from 'selenium-webdriver'
+import {USERNAME, PASSWORD} from './.env'
 
 const driver = new webdriver.Builder().forBrowser('chrome').build()
+const loginPath =
+	'//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[1]/input'
+const passwordPath =
+	'//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/input'
+const loginButtonPath =
+	'//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/span[1]/button'
 
 driver.get('http://www.instagram.com')
 driver.findElement(By.className('_fcn8k')).click()
-// driver.findElement(By.name('btnG')).click()
-// driver.wait(until.titleIs('webdriver - Google Search'), 10000)
-// driver.quit()
+
+const login = driver.findElement(By.xpath(loginPath))
+const password = driver.findElement(By.xpath(passwordPath))
+const loginButton = driver.findElement(By.xpath(loginButtonPath))
+driver.wait(until.elementIsVisible(login), 100)
+login.sendKeys(USERNAME)
+password.sendKeys(PASSWORD)
+loginButton.click()
