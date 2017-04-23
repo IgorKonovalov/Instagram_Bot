03b591646d5ee97e0d22f3f09e9029a1e21b14a8
Igor Konovalov <konovalov.avp@gmail.com>
2017-04-23 19:53:05 +0300

    no uncommited

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // tags by which we want to like
 
-export const TAGS = ['mountains', 'dogs', 'saint-petersburg', 'programming']
+export const TAGS = ['honey', 'geometry', 'abstract']
 
 export const loginPath =
 	'//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[1]/input'
--- src/likeByTags.js
+++ src/likeByTags.js
@@ -2,7 +2,7 @@ import {TAGS} from './config'
 import {By, until, promise, sleep} from 'selenium-webdriver'
 
 export default function(driver) {
-  let likes = 0
+	let likes = 0
 	TAGS.forEach(tag => {
 		driver.sleep(2000)
 		driver.get(`https://www.instagram.com/explore/tags/${tag}`)
@@ -24,13 +24,13 @@ export default function(driver) {
 							)
 							.click()
 						driver.findElement(By.className('_3eajp')).click()
-            likes ++
+						likes++
+						console.log(tag, likes)
 					})
 				})
 				.catch(err => console.log(err))
 		})
 	})
-  console.log(likes)
 }
 
 // TAGS.forEach(tag => {
